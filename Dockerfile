b21eb5e769082697bf323f8f2fe55d31a313971d
Tomas Jancar <jancar@modpreneur.com>
2016-06-19 20:41:28 +0200

    docker for prod.

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,17 @@
-docker/dev/Dockerfile
+FROM modpreneur/apache-framework:0.4
+
+MAINTAINER Tomáš Jančar <jancar@modpreneur.com>
+
+RUN docker-php-ext-install pdo pdo_mysql
+
+RUN  apt-get install python-setuptools -y \
+  && easy_install supervisor
+
+# Install app
+ADD . /var/app
+
+EXPOSE 8666
+EXPOSE 9005
+
+RUN chmod +x entrypoint.sh
+ENTRYPOINT ["sh", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,15 @@
-docker/dev/entrypoint.sh
+#!/bin/bash sh
+
+mkdir -p /var/app/var/cache
+mkdir -p /var/app/var/logs
+
+chmod -R 0777 /var/app/var/logs
+chmod -R 0777 /var/app/var/cache
+
+supervisord -c /var/app/supervisor/supervisord.conf
+supervisorctl -c /var/app/supervisor/supervisord.conf status
+
+composer install
+composer run-script post-install-cmd --no-interaction
+
+exec apache2-foreground
