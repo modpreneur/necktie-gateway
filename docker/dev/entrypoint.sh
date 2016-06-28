100b03b8b84e0e1cee2b2aaa5ca4e9c45b0de3c5
Tomas Jancar <jancar@modpreneur.com>
2016-06-28 18:10:49 +0200

    edits...

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -9,9 +9,9 @@ chmod -R 0777 /var/app/var/cache
 composer install
 composer run-script post-install-cmd --no-interaction
 
+php /var/app/bin/console bunny:setup
+
 supervisord -c /var/app/supervisor/supervisord.conf
 supervisorctl -c /var/app/supervisor/supervisord.conf status
-
-php /var/app/bin/console bunny:setup
 
 exec apache2-foreground
