73abac235c59c6b2595e25f18f09a505f2d8923c
Martin Kolek <kolek@modpreneur.com>
2017-02-28 18:48:33 +0100

    gw to php fpm and nginx

--- docker/phpFpm/nuc/entrypoint.sh
+++ docker/phpFpm/nuc/entrypoint.sh
@@ -4,15 +4,17 @@ mkdir -p /var/app/var/cache
 mkdir -p /var/app/var/logs
 mkdir -p /var/app/var/logs/supervisord
 
-#bin/console redis:flushdb
+composer install --no-scripts --no-suggest --optimize-autoloader
+#composer run-script post-install-cmd --no-interaction
 
-composer run-script post-install-cmd --no-interaction
-
-##supervisor - load config from necktie
+#supervisor - load config from necktie
 ENV=prod supervisord -c /var/app/supervisor/supervisord.conf
 supervisorctl -c /var/app/supervisor/supervisord.conf reload
 
-bin/console necktie:bunny:setup
+chmod -R 0777 /var/app/var/cache
+chmod -R 0777 /var/app/var/logs
+
+bin/console bunny:setup
 
 chmod -R 0777 /var/app/var/cache
 chmod -R 0777 /var/app/var/logs
