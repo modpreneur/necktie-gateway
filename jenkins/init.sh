37abe4413820df91d683ddd399116accfaea8661
Martin Kolek <kolek@modpreneur.com>
2017-03-01 11:02:46 +0100

    clean init.sh

--- jenkins/init.sh
+++ jenkins/init.sh
@@ -11,28 +11,11 @@ supervisorctl -c /var/app/supervisor/supervisord.conf status
 
 bin/console bunny:setup
 
-#cd /var/app
-#composer install
-#
-#/var/app/bin/console doctrine:schema:update --force
-#
-#ENV=dev supervisord -c /var/app/supervisor/supervisord.conf
-#supervisorctl -c /var/app/supervisor/supervisord.conf reload
-#
-
 chown -R 106 /var/app/
 chmod -R 0777 /var/app/var/cache
 chmod -R 0777 /var/app/var/logs
 chown -R www-data:www-data /var/app/var/cache
 chown -R www-data:www-data /var/app/var/logs
 
-
-#wait for supervisor to start (tests result in error "expected RUNNING, got STARTING instead" otherwise)
-#sleep 5
-
 mkdir /var/app/build
-/var/app/bin/phpunit -c /var/app/phpunit.xml /var/app/src --log-junit /var/app/build/phpunit.xml
-
-
-#rm -R /var/app/var/cache/*
-#rm -R /var/app/vendor/*
+bin/phpunit -c /var/app/phpunit.xml /var/app/src --log-junit /var/app/build/phpunit.xml
