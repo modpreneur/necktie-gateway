58dafd4d919f2df33f75ed992cd9e91b4669c385
Gabriel <bordovsky.gabriel@gmail.com>
2017-03-01 07:58:24 +0100

    not good fix of  tests

--- jenkins/init.sh
+++ jenkins/init.sh
@@ -5,6 +5,9 @@ composer update
 
 /var/app/bin/console doctrine:schema:update --force
 
+ENV=dev supervisord -c /var/app/supervisor/supervisord.conf
+supervisorctl -c /var/app/supervisor/supervisord.conf reload
+
 mkdir /var/app/build
 /var/app/bin/phpunit -c /var/app/phpunit.xml /var/app/src --log-junit /var/app/build/phpunit.xml
 
@@ -12,6 +15,9 @@ chown -R 106 /var/app/
 chown -R www-data:www-data /var/app/var/logs
 chown -R www-data:www-data /var/app/var/cache
 chmod -R 777 /var/app/var/cache
+
+
+
 #rm -R /var/app/var/cache/*
 #rm -R /var/app/vendor/*
 
