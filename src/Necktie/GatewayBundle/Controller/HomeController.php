bfaeeca5bb4438f4f7a2f6a7444e2efeeaba7016
Tomáš Jančar <jancar@modpreneur.com>
2017-03-17 13:28:37 +0100

    Add homepage

--- src/Necktie/GatewayBundle/Controller/HomeController.php
+++ src/Necktie/GatewayBundle/Controller/HomeController.php
@@ -2,11 +2,21 @@
 
 namespace Necktie\GatewayBundle\Controller;
 
+use Necktie\GatewayBundle\Entity\Message;
+use Necktie\GatewayBundle\Entity\SystemLog;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+
+use Supervisor\Supervisor;
+use Supervisor\Connector\XmlRpc;
+use fXmlRpc\Client as ClientRpc;
+use GuzzleHttp\Client;
+use fXmlRpc\Transport\HttpAdapterTransport;
+use Http\Adapter\Guzzle6\Client as ClientGuzzle;
+use Http\Message\MessageFactory\DiactorosMessageFactory;
 
 /**
  * Class HomeController
@@ -14,27 +24,102 @@ use Symfony\Component\Process\Process;
  */
 class HomeController extends Controller
 {
+    protected $supervisor;
 
-    //@todo @TomasJancar -> secure supervisor
 
     /**
-     * @return JsonResponse
+     * HomeController constructor.
+     *
+     */
+    public function __construct()
+    {
+        $this->supervisor = new Supervisor($this->getConnector());
+    }
+
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction()
     {
-        $process = new Process('supervisorctl -c /var/app/supervisor/supervisord.conf status');
-        $process->run();
+        $em = $this->get('doctrine.orm.entity_manager');
+        $messages = $em
+            ->getRepository(Message::class)
+            ->findBy([], ['id' => 'desc'], 20);
 
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+        $systemLogs = $em
+            ->getRepository(SystemLog::class)
+            ->findBy([], ['id' => 'desc'], 20);
+
+        return $this->render('GatewayBundle:Home:index.html.twig', [
+            'version'    => exec("git rev-parse --verify HEAD"),
+            'datetime'   => new \DateTime(),
+            'rabbitUrl'  => $this->getParameter('rabbit_url'),
+            'supervisorCommands' => $this->getProcesses(),
+            'messages'   => $messages,
+            'systemLogs' => $systemLogs
+        ]);
+    }
+
+
+    /**
+     * @param string $group
+     * @param string $name
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function stopProcessAction(string $group, string $name)
+    {
+        $this->supervisor->stopProcess($group . ':' . $name, 10);
+
+        return $this->redirectToRoute('gateway');
+    }
+
+
+    /**
+     * @param string $group
+     * @param string $name
+     *
+     * @return Response
+     */
+    public function processLogsAction(string $group, string $name)
+    {
+        $result = '';
+
+        foreach ($this->supervisor->tailProcessStdoutLog($group . ':' . $name, 0, -1) as $item) {
+            $result .= $item . PHP_EOL;
         }
 
-        $r = $process->getOutput();
-
-        $r = explode(PHP_EOL, $r);
-
-        return new JsonResponse($r);
+        return new Response($result);
     }
+
+
+    /**
+     * @param string $group
+     * @param string $name
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function cleanLogAction(string $group, string $name)
+    {
+        $this->supervisor->clearProcessLogs($group . ':' . $name);
+        return $this->redirectToRoute('gateway');
+    }
+
+
+    /**
+     * @param string $group
+     * @param string $name
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function startProcessAction(string $group, string $name)
+    {
+        $this->supervisor->startProcess($group . ':' . $name, 10);
+
+        return $this->redirectToRoute('gateway');
+    }
+
 
 
     /**
@@ -48,9 +133,37 @@ class HomeController extends Controller
             throw new ProcessFailedException($process);
         }
 
-        $r = $process->getOutput();
-        $r = explode(PHP_EOL, $r);
+        //$r = $process->getOutput();
+        $r = $this->getProcesses();
 
-        return new JsonResponse($r);
+        return $this->redirectToRoute('gateway');
+    }
+
+
+    /**
+     * @return \Supervisor\Process[]
+     */
+    private function getProcesses() : array
+    {
+        return $this->supervisor->getAllProcesses();
+    }
+
+
+    /**
+     * @return XmlRpc
+     */
+    private function getConnector(): XmlRpc
+    {
+        $httpClient = new Client(['auth' => ['user', '123']]);
+
+        $client = new ClientRpc(
+            'localhost' . ':' . '9005' . '/RPC2',
+            new HttpAdapterTransport(
+                new DiactorosMessageFactory(),
+                new ClientGuzzle($httpClient)
+            )
+        );
+
+        return new XmlRpc($client);
     }
 }
